f8319371c6e9f525639d0ecce36b1f23627fff24
pramitsingh0 <pramitsingh0@gmail.com>
2022-10-04 10:28:57 +0530

    operate function completed

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case 'add':
+      add(num1, num2);
+      break;
+    case 'substract':
+      substract(num1, num2);
+      break;
+    case 'multiply':
+      multiply(num1, num2);
+      break;
+    case 'divide':
+      divide(num1, num2);
+    default:
+      return "Input an operator and two numbers"
+  }
+}
